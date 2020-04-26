f2126e8e729702024cc4c651917794291c1e91f5
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-04-25 21:39:18 -0400

    resolved browser problems

--- src/wfc/main.js
+++ src/wfc/main.js
@@ -1,5 +1,3 @@
-"use strict";
-exports.__esModule = true;
-var sprite_js_1 = require("./sprite.js");
-var s = new sprite_js_1["default"](2, 2);
+import Sprite from "./sprite.js";
+var s = new Sprite(2, 2);
 console.log(s);
--- src/wfc/wfc.ts
+++ src/wfc/wfc.ts
@@ -14,15 +14,16 @@ export default class WFC {
         var offsetX = 0;
         var offsetY = 0;
         if(!this.sprite.wrapSprite) {
-            offsetX = this.sprite.width;
-            offsetY = this.sprite.height;
+            offsetX = (sliceWidth > 0) ? sliceWidth-1 : 0;
+            offsetY = (sliceHeight > 0) ? sliceHeight-1 : 0;
         }
 
         var tiles = new Array<Sprite>();
 
         for (var j = 0; j < this.sprite.height-offsetY; j++) {
             for (var i = 0; i < this.sprite.width-offsetX; i++) {
-                tiles.push(this.sprite.slice(i,j,sliceWidth,sliceWidth));
+                
+                tiles.push(this.sprite.slice(i,j,sliceWidth,sliceHeight));
             }
         }
         return tiles;
